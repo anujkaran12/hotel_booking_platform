015d396946a5d57e7f9c1114ad1499cc97df42d1
Anuj Karan <anuj12082004dev@gmail.com>
2026-05-30 17:14:17 +0530

    feat: added home route response for backend health

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,20 @@ import { bookingRoutes } from "./modules/bookings/booking.routes";
 import { notificationRoutes } from "./modules/notifications/notification.routes";
 import { paymentRoutes } from "./modules/payments/payment.routes";
 import { generalLimiter } from "./middlewares/rateLimit.middleware";
+import { successResponse } from "./utils/apiResponse";
 
 export const app = express();
 
 app.use(express.json());
 app.use(generalLimiter);
 
+app.get("/", (req, res) => {
+  successResponse(
+    res,
+    200,
+    "Hotle Booking Platform Management Backend is live now",
+  );
+});
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/hotel", hotelRoutes);
 app.use("/api/v1", roomRoutes);
@@ -31,4 +39,3 @@ mongoose
     app.listen(3000, () => console.log("Server running on port 3000"));
   })
   .catch((err) => console.log(err));
-
