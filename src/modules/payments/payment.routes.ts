2dda120698e0707b9ae396b40484f7b6b20a8323
Anuj Karan <anuj12082004dev@gmail.com>
2026-05-30 16:27:22 +0530

    feat: added payment-rate-limiter to payment routes

--- src/modules/payments/payment.routes.ts
+++ src/modules/payments/payment.routes.ts
@@ -2,24 +2,28 @@ import { Router } from "express";
 import * as paymentController from "./payment.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 import { roleMiddleware } from "../../middlewares/role.middleware";
+import { paymentLimiter } from "../../middlewares/rateLimit.middleware";
 
 export const paymentRoutes = Router();
 
 // customer
 paymentRoutes.post(
   "/initiate",
+  paymentLimiter,
   authMiddleware,
   roleMiddleware("customer"),
   paymentController.initiatePayment,
 );
 paymentRoutes.post(
   "/verify",
+  paymentLimiter,
   authMiddleware,
   roleMiddleware("customer"),
   paymentController.verifyPayment,
 );
 paymentRoutes.post(
   "/refund",
+  paymentLimiter,
   authMiddleware,
   roleMiddleware("customer"),
   paymentController.refundPayment,
@@ -31,4 +35,4 @@ paymentRoutes.get(
 );
 
 // webhook: no auth because Razorpay verifies using signature
-paymentRoutes.post("/webhook", paymentController.handleWebhook);
+paymentRoutes.post("/webhook", paymentLimiter, paymentController.handleWebhook);
