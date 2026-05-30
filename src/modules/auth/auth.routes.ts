e19583189e965130af361d82b357103feb6aa918
Anuj Karan <anuj12082004dev@gmail.com>
2026-05-30 16:26:55 +0530

    feat: added auth-rate-limiter to auth-routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register, login, getMe } from "./auth.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
+import { authLimiter } from "../../middlewares/rateLimit.middleware";
 
 export const authRoutes = Router();
 
-authRoutes.post("/register", register);
-authRoutes.post("/login", login);
+authRoutes.post("/register", authLimiter, register);
+authRoutes.post("/login", authLimiter, login);
 authRoutes.get("/me", authMiddleware, getMe);
